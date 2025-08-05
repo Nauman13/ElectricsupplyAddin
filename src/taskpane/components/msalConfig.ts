6886876a8e63baa85aa2719575b45da0900ef23d
Hassnat58 <Hussnainsajjad58@gmail.com>
2025-08-06 02:06:35 +0500

    Changes

--- src/taskpane/components/msalConfig.ts
+++ src/taskpane/components/msalConfig.ts
@@ -2,9 +2,9 @@ import { Configuration } from "@azure/msal-browser";
 
 export const msalConfig: Configuration = {
   auth: {
-    clientId: "a8f0615d-19b4-4c1d-b578-3f754d19e56c", 
+    clientId: "a8f0615d-19b4-4c1d-b578-3f754d19e56c",
     authority: "https://login.microsoftonline.com/common",
-    redirectUri: "https://localhost:3000/",
+    redirectUri: "https://electricsupply-addin.vercel.app/",
   },
   cache: {
     cacheLocation: "localStorage",
