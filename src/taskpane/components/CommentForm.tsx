3142770ca8ad10cc8a38bb3473b68fcd61c7d82c
Hassnat58 <Hussnainsajjad58@gmail.com>
2025-08-12 06:07:10 +0500

    id match

--- src/taskpane/components/CommentForm.tsx
+++ src/taskpane/components/CommentForm.tsx
@@ -238,8 +238,10 @@ const CommentForm: React.FC = () => {
       const graphToken = await getGraphToken();
       const spToken = await getSharePointToken();
 
-      const emailIdValue = encodeURIComponent(id);
-      const url = `https://graph.microsoft.com/v1.0/sites/${siteId}/lists/${listId}/items?expand=fields&$filter=fields/EmailID eq '${emailIdValue}'&$orderby=createdDateTime asc`;
+      const emailIdValue = id.replace(/'/g, "''");
+      const url =
+        `https://graph.microsoft.com/v1.0/sites/${siteId}/lists/${listId}/items` +
+        `?expand=fields&$filter=fields/EmailID eq '${emailIdValue}'&$orderby=createdDateTime asc`;
 
       const response = await fetch(url, {
         headers: { Authorization: `Bearer ${graphToken}` },
